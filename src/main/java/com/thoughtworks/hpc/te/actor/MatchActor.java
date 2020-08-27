6be1ea2328ced2fc64f431f29b5eee370702fbd6
superxcgm <superxcgm@gmail.com>
2020-08-27 17:48:13 +0800

    Remove useless context and add logger

--- src/main/java/com/thoughtworks/hpc/te/actor/MatchActor.java
+++ src/main/java/com/thoughtworks/hpc/te/actor/MatchActor.java
@@ -2,24 +2,30 @@ package com.thoughtworks.hpc.te.actor;
 
 import akka.actor.typed.Behavior;
 import akka.actor.typed.eventstream.EventStream;
+import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
 import akka.actor.typed.receptionist.Receptionist;
 import akka.actor.typed.receptionist.ServiceKey;
 import com.google.protobuf.Timestamp;
 import com.thoughtworks.hpc.te.controller.Order;
 import com.thoughtworks.hpc.te.controller.Trade;
 import com.thoughtworks.hpc.te.controller.TradingSide;
+import org.slf4j.Logger;
 
-public class MatchActor {
+public class MatchActor extends AbstractBehavior<Order> {
+    private Logger logger;
 
-    // Todo: 这里可能没必要保存context
-    private final ActorContext<Order> context;
+    private MatchActor(ActorContext<Order> context) {
+        super(context);
+        logger = getContext().getLog();
+    }
 
     public static Behavior<Order> create(int symbolId) {
         return Behaviors.setup(context -> {
             context.getSystem().receptionist().tell(Receptionist.register(generateServiceKey(symbolId), context.getSelf()));
-            return new MatchActor(context).behavior();
+            return new MatchActor(context);
         });
     }
 
@@ -27,16 +33,9 @@ public class MatchActor {
         return ServiceKey.create(Order.class, "symbol_" + symbolId);
     }
 
-    private MatchActor(ActorContext<Order> context) {
-        this.context = context;
-    }
-
-    private Behavior<Order> behavior() {
-        return Behaviors.receive(Order.class).onMessage(Order.class, this::match).build();
-    }
 
     private Behavior<Order> match(Order order) {
-        context.getLog().info("MatchActor handle order {}", order);
+        logger.info("MatchActor handle order {}", order);
         // Todo: 撮合逻辑
         Trade trade = Trade.newBuilder()
                 .setMakerId(1)
@@ -49,10 +48,15 @@ public class MatchActor {
                 .setSymbolId(7)
                 .setDealTime(Timestamp.getDefaultInstance())
                 .build();
-        context.getLog().info("Match success, trade {}", trade);
+        logger.info("Match success, trade {}", trade);
 
-        context.getSystem().eventStream().tell(new EventStream.Publish<>(trade));
+        getContext().getSystem().eventStream().tell(new EventStream.Publish<>(trade));
 
         return Behaviors.same();
+    }
+
+    @Override
+    public Receive<Order> createReceive() {
+        return newReceiveBuilder().onMessage(Order.class, this::match).build();
     }
 }
