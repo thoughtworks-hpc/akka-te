a138137c5d256b9af1cbddc11fbdcbd3e06cd06b
superxcgm <superxcgm@gmail.com>
2020-08-28 14:54:32 +0800

    should_generate_correct_trade_given_head_sell_price_less_than_buy_order_and_have_same_amount_when_match_buy_order

--- src/main/java/com/thoughtworks/hpc/te/actor/MatchActor.java
+++ src/main/java/com/thoughtworks/hpc/te/actor/MatchActor.java
@@ -14,12 +14,33 @@ import com.thoughtworks.hpc.te.controller.Trade;
 import com.thoughtworks.hpc.te.controller.TradingSide;
 import org.slf4j.Logger;
 
+import java.util.PriorityQueue;
+
 public class MatchActor extends AbstractBehavior<Order> {
-    private Logger logger;
+    private final Logger logger;
+    private PriorityQueue<Order> buyOrderQueue;
+    private PriorityQueue<Order> sellOrderQueue;
 
     private MatchActor(ActorContext<Order> context) {
         super(context);
         logger = getContext().getLog();
+
+        buyOrderQueue = new PriorityQueue<>((o1, o2) -> {
+            if (o1.getPrice() != o2.getPrice()) {
+                // price DESC
+                return o2.getPrice() - o1.getPrice();
+            }
+            // time ASC
+            return (int) (o1.getSubmitTime().getSeconds() - o2.getSubmitTime().getSeconds());
+        });
+        sellOrderQueue = new PriorityQueue<>(((o1, o2) -> {
+            if (o1.getPrice() != o2.getPrice()) {
+                // price ASC
+                return o1.getPrice() - o2.getPrice();
+            }
+            // time ASC
+            return (int) (o1.getSubmitTime().getSeconds() - o2.getSubmitTime().getSeconds());
+        }));
     }
 
     public static Behavior<Order> create(int symbolId) {
@@ -37,23 +58,69 @@ public class MatchActor extends AbstractBehavior<Order> {
     private Behavior<Order> match(Order order) {
         logger.info("MatchActor handle order {}", order);
         // Todo: 撮合逻辑
+        Order buy;
+        Order sell;
+
+        if (order.getTradingSide() == TradingSide.TRADING_BUY) {
+            buy = order;
+            sell = sellOrderQueue.peek();
+        } else {
+            sell = order;
+            buy = buyOrderQueue.peek();
+        }
+
+        if (buy == null || sell == null) {
+            logger.info("Opposite order queue is empty. add order {} to queue.", order.getOrderId());
+            addOrderToQueue(order);
+            return Behaviors.same();
+        }
+
+        if (buy.getPrice() < sell.getPrice()) {
+            logger.info("Buy order price [{}] less than sell order[{}].", buy.getPrice(), sell.getPrice());
+            addOrderToQueue(order);
+            return Behaviors.same();
+        }
+
+        // buy price >= sell price
+        if (buy.getAmount() == sell.getAmount()) {
+            Trade trade = Trade.newBuilder()
+                    .setMakerId(order == buy ? sell.getOrderId() : buy.getOrderId())
+                    .setTakerId(order.getOrderId())
+                    .setTradingSide(order.getTradingSide())
+                    .setAmount(order.getAmount())
+                    .setPrice(order == buy ? sell.getPrice() : buy.getPrice())
+                    .setSellerUserId(sell.getUserId())
+                    .setBuyerUserId(buy.getUserId())
+                    .setSymbolId(order.getSymbolId())
+                    .setDealTime(generateCurrentTimestamp())
+                    .build();
+            if (order == buy) {
+                sellOrderQueue.poll();
+            } else {
+                buyOrderQueue.poll();
+            }
+            logger.info("Match success, trade {}", trade);
+            getContext().getSystem().eventStream().tell(new EventStream.Publish<>(trade));
+            return Behaviors.same();
+        }
+
         return Behaviors.same();
-//        Trade trade = Trade.newBuilder()
-//                .setMakerId(1)
-//                .setTakerId(2)
-//                .setTradingSide(TradingSide.TRADING_BUY)
-//                .setAmount(3)
-//                .setPrice(4)
-//                .setSellerUserId(5)
-//                .setBuyerUserId(6)
-//                .setSymbolId(7)
-//                .setDealTime(Timestamp.getDefaultInstance())
-//                .build();
-//        logger.info("Match success, trade {}", trade);
-//
-//        getContext().getSystem().eventStream().tell(new EventStream.Publish<>(trade));
-//
-//        return Behaviors.same();
+    }
+
+    private Timestamp generateCurrentTimestamp() {
+        long millis = System.currentTimeMillis();
+        return Timestamp.newBuilder()
+                .setSeconds(millis / 1000)
+                .setNanos((int) ((millis % 1000) * 1000_000))
+                .build();
+    }
+
+    private void addOrderToQueue(Order order) {
+        if (order.getTradingSide() == TradingSide.TRADING_BUY) {
+            buyOrderQueue.add(order);
+        } else {
+            sellOrderQueue.add(order);
+        }
     }
 
     @Override
--- src/test/java/com/thoughtworks/hpc/te/actor/MatchActorTest.java
+++ src/test/java/com/thoughtworks/hpc/te/actor/MatchActorTest.java
@@ -7,6 +7,7 @@ import akka.actor.typed.eventstream.EventStream;
 import com.thoughtworks.hpc.te.controller.Order;
 import com.thoughtworks.hpc.te.controller.Trade;
 import com.thoughtworks.hpc.te.controller.TradingSide;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -58,5 +59,49 @@ public class MatchActorTest {
         matchActor.tell(Order.newBuilder().setTradingSide(TradingSide.TRADING_SELL).setPrice(6).build());
 
         probe.expectNoMessage();
+    }
+
+    @Test
+    public void should_generate_correct_trade_given_head_sell_price_less_than_buy_order_and_have_same_amount_when_match_buy_order() {
+        TestProbe<Trade> probe = testKit.createTestProbe();
+        testKit.system().eventStream().tell(new EventStream.Subscribe<>(Trade.class, probe.ref()));
+        ActorRef<Order> matchActor = testKit.spawn(MatchActor.create(1));
+        Order sellOder = Order.newBuilder()
+                .setOrderId(1)
+                .setSymbolId(1)
+                .setUserId(1)
+                .setTradingSide(TradingSide.TRADING_SELL)
+                .setPrice(3)
+                .setAmount(3)
+                .build();
+        Order buyOrder = Order.newBuilder()
+                .setOrderId(2)
+                .setSymbolId(1)
+                .setUserId(2)
+                .setTradingSide(TradingSide.TRADING_BUY)
+                .setPrice(5)
+                .setAmount(3)
+                .build();
+
+        matchActor.tell(sellOder);
+        matchActor.tell(buyOrder);
+
+        Trade wantTrade = Trade.newBuilder()
+                .setMakerId(sellOder.getOrderId())
+                .setTakerId(buyOrder.getOrderId())
+                .setTradingSide(buyOrder.getTradingSide())
+                .setAmount(buyOrder.getAmount())
+                .setPrice(sellOder.getPrice())
+                .setSellerUserId(sellOder.getUserId())
+                .setBuyerUserId(buyOrder.getUserId())
+                .setSymbolId(buyOrder.getSymbolId())
+                .build();
+        Trade gotTrade = probe.expectMessageClass(Trade.class);
+        assertTradeEquals(wantTrade, gotTrade);
+    }
+
+    private void assertTradeEquals(Trade want, Trade got) {
+        Trade wantOverwriteDealTime = want.toBuilder().setDealTime(got.getDealTime()).build();
+        Assert.assertEquals(wantOverwriteDealTime, got);
     }
 }
