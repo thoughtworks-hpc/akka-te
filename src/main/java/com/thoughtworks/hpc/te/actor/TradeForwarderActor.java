7d949e773fd675f195cf564a715e5acf5a339220
superxcgm <superxcgm@gmail.com>
2020-08-31 17:43:00 +0800

    Close gRPC connection when actor down

--- src/main/java/com/thoughtworks/hpc/te/actor/TradeForwarderActor.java
+++ src/main/java/com/thoughtworks/hpc/te/actor/TradeForwarderActor.java
@@ -2,6 +2,7 @@ package com.thoughtworks.hpc.te.actor;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
@@ -9,11 +10,12 @@ import akka.actor.typed.javadsl.Receive;
 import akka.actor.typed.pubsub.Topic;
 import com.thoughtworks.hpc.te.controller.Trade;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
 
 public class TradeForwarderActor extends AbstractBehavior<Trade> {
 
     private final StreamObserver<Trade> responseObserver;
-    private final ActorRef<Topic.Command<Trade>> topic;
+    private final Logger logger;
 
     public static Behavior<Trade> create(StreamObserver<Trade> responseObserver, ActorRef<Topic.Command<Trade>> topic) {
         return Behaviors.setup(context -> new TradeForwarderActor(context, responseObserver, topic));
@@ -22,22 +24,35 @@ public class TradeForwarderActor extends AbstractBehavior<Trade> {
     private TradeForwarderActor(ActorContext<Trade> context, StreamObserver<Trade> responseObserver, ActorRef<Topic.Command<Trade>> topic) {
         super(context);
         this.responseObserver = responseObserver;
-        this.topic = topic;
+        this.logger = getContext().getLog();
 
         topic.tell(Topic.subscribe(getContext().getSelf()));
     }
 
     @Override
     public Receive<Trade> createReceive() {
-        return newReceiveBuilder().onMessage(Trade.class, this::onTrade).build();
+        return newReceiveBuilder()
+                .onMessage(Trade.class, this::onTrade)
+                .onSignal(PostStop.class, signal -> onPostStop())
+                .build();
+    }
+
+    private Behavior<Trade> onPostStop() {
+        // close gRPC connection when actor down
+        try {
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            // ignore
+            logger.warn("close connection failed. error=" + e);
+        }
+        return this;
     }
 
     private Behavior<Trade> onTrade(Trade trade) {
         responseObserver.onNext(trade);
         // Todo: 这里需要处理如果客户端断开了的情况
-        getContext().getLog().info("Send trade {} to client", trade);
+        logger.info("Send trade {} to client", trade);
         return this;
     }
 
-    // Todo: actor挂掉时，需要调用responseObserver.onCompleted()
 }
